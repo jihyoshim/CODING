40addf745e73ef78485d3df289ab19751aa273d9
jihyoshim <jihyoshim1027@gmail.com>
2026-02-21 16:55:48 +0900

    [Silver III] Title: 주유소, Time: 120 ms, Memory: 136212 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,10 @@
+n = int(input())
+distance = list(map(int, input().split()))
+price = list(map(int, input().split()))
+min_price = price[0] 
+ans = 0
+for i in range(n-1) : 
+  if price[i] < min_price :
+    min_price = price[i] 
+  ans += min_price * distance[i] 
+print(ans)
